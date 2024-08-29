d36a6b0eefdda2e15d01ae0885136890d0484587
Ankur2024 <ag3942280@gmail.com>
2024-08-29 18:26:21 +0530

    See Manual Dependency Injection And Its Drawback

--- Dagger Hilt/app/src/main/java/com/example/daggerhillt/MainActivity.kt
+++ Dagger Hilt/app/src/main/java/com/example/daggerhillt/MainActivity.kt
@@ -7,21 +7,28 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.daggerhillt.manualdependecy.BaseApp
+import com.example.daggerhillt.manualdependecy.di.DownloadFactory
+import com.example.daggerhillt.manualdependecy.di.Downloader
 import com.example.daggerhillt.ui.theme.DaggerHilltTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var baseApp: BaseApp
+    private val download = DownloadFactory.create()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             DaggerHilltTheme {
+                baseApp = BaseApp()
+                baseApp.car.getCar()
+                download.getDownload()
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +38,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting( modifier: Modifier = Modifier) {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     DaggerHilltTheme {
-        Greeting("Android")
     }
 }
---
+++ Dagger Hilt/app/src/main/java/com/example/daggerhillt/manualdependecy/BaseApp.kt
@@ -0,0 +1,11 @@
+package com.example.daggerhillt.manualdependecy
+
+import android.app.Application
+import com.example.daggerhillt.manualdependecy.di.Car
+import com.example.daggerhillt.manualdependecy.di.Engine
+import com.example.daggerhillt.manualdependecy.di.Wheel
+
+class BaseApp: Application() {
+
+    var car = Car(Engine(), Wheel())
+}
